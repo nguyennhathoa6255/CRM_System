c8f6a4671ea2d4a8e68928c1324fb5897eb91f9f
nguyennhathoa6255 <nguyennhathoa6255@gmail.com>
2023-12-27 10:37:43 +0700

    cx

--- core/settings.py
+++ core/settings.py
@@ -109,7 +109,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = '/var/app/static'
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [BASE_DIR / "static", "/var/app/static"]
 
 LOGIN_URL='/signin'
 LOGIN_REDIRECT_URL='/signin'
